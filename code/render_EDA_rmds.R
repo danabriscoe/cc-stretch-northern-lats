54419c460dfa78496c3b4449d2add506dea0b2c3
danabriscoe <danakbriscoe@gmail.com>
2023-10-24 00:41:47 +1300

    render qmd to html

--- code/render_EDA_rmds.R
+++ code/render_EDA_rmds.R
@@ -1,8 +1,9 @@
 ## render rmd script to github docs/html
 
 rmarkdown::render(
-    # "../code/02_plot_SST_ts.Rmd",
-    "rmd/northern_lats_EDA_20Sept2023.Rmd",
-    # "code/02_plot_SST_ts.Rmd",
+    # # "../code/02_plot_SST_ts.Rmd",
+    # "rmd/northern_lats_EDA_20Sept2023.Rmd",
+    "northern_lats_EDA_20Oct2023.qmd",
+    # # "code/02_plot_SST_ts.Rmd",
     output_file = "../docs/index.html"
 )
