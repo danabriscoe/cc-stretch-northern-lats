b44cc1409d90b6d47ab6380c379ff99d451a5c96
danabriscoe <danakbriscoe@gmail.com>
2023-10-24 08:53:59 +1300

    manual revert to working rmd render

--- code/render_EDA_rmds.R
+++ code/render_EDA_rmds.R
@@ -1,8 +1,8 @@
 ## render rmd script to github docs/html
 
 rmarkdown::render(
-    # "rmd/northern_lats_EDA_20Sept2023.Rmd",
-    "northern_lats_EDA_20Oct2023.qmd",
-    # output_file = "../docs/index.html"
-    output_file = "../docs/index.qmd"
+    "rmd/northern_lats_EDA_20Sept2023.Rmd",
+    # "northern_lats_EDA_20Oct2023.qmd",
+    output_file = "../docs/index.html"
+    # output_file = "../docs/index.qmd"
 )
