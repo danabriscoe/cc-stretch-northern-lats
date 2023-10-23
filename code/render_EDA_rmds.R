a1571f570ee8e39e032a13b95e54847a3087a335
danabriscoe <danakbriscoe@gmail.com>
2023-10-24 01:05:48 +1300

    update to save index.qmd

--- code/render_EDA_rmds.R
+++ code/render_EDA_rmds.R
@@ -1,9 +1,8 @@
 ## render rmd script to github docs/html
 
 rmarkdown::render(
-    # # "../code/02_plot_SST_ts.Rmd",
     # "rmd/northern_lats_EDA_20Sept2023.Rmd",
     "northern_lats_EDA_20Oct2023.qmd",
-    # # "code/02_plot_SST_ts.Rmd",
-    output_file = "../docs/index.html"
+    # output_file = "../docs/index.html"
+    output_file = "../docs/index.qmd"
 )
